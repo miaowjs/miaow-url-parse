72f26ce6f3524587f5f99d0884b963f6d3c5231f
anhulife <anhulife@gmail.com>
2015-06-30 17:45:19 +0800

    回滚相对路径的修改

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function parse(option, cb) {
         return cb(err);
       }
 
-      urlMap[result[1]] = module.url || module.destPathWithHash;
+      urlMap[result[1]] = module.url || path.relative(path.dirname(this.destAbsPath), module.destAbsPathWithHash);
       cb();
     }.bind(this));
   }.bind(this), function (err) {
